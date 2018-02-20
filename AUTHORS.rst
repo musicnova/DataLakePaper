3841a25d859e834a1777795a8cf22bee499391b3
user <user@hadoop-m2@mos.ru>
2018-02-20 18:40:47 +0300

    003: init commit

---
+++ AUTHORS.rst
@@ -0,0 +1,12 @@
+This project was started by User. In 2018 MusicNova became
+co-maintainer of the project.
+
+It was initiallay *heavily* inspired by the PHPExcel library:
+http://www.phpexcel.net/
+
+Thanks to all those who participate in the project (in alphabetical order):
+
+* User
+* MusicNova
+
+Project logo designed by User, font by MusicNova
--- datalakeplan/VaultExcel.py
+++ datalakeplan/VaultExcel.py
@@ -1,28 +1,96 @@
 """Excel module"""
- 
-def create_project():
-    from openpyxl import Workbook
-    wb = Workbook()
+class VaultExcel :
 
-    # grab the active worksheet
-    ws = wb.active
+    def __init__(self) :
+        pass
 
-    # Data can be assigned directly to cells
-    ws['A1'] = 42
 
-    # Rows can also be appended
-    ws.append([1, 2, 3])
 
-    # Python types will automatically be converted
-    import datetime
-    ws['A2'] = datetime.datetime.now()
+    def __before_create_project(workdir):
+        # Debug output
+        print("before create project")
+        
+        import pathlib
+        pathlib.Path(workdir).mkdir(parents=True, exist_ok=True)
 
-    # Save the file
-    wb.save("sample.xlsx")
 
-    # Debug output
-    print("ok")
+
+    def create_project(workdir):
+        VaultExcel.__before_create_project(workdir)
+    
+        from openpyxl import Workbook
+        wb = Workbook()
+
+        # grab the active worksheet
+        ws = wb.active
+        ws.title = "V1.Changelog"
+        ws.sheet_properties.tabColor = "007700"
+        
+        # W sheets
+        wb.create_sheet("W1.Information")
+        ws2 = wb["W1.Information"]
+        ws2.sheet_properties.tabColor = "118800"
+        
+        # X sheets
+        wb.create_sheet("X1.Tables")
+        ws3 = wb["X1.Tables"]
+        ws3.sheet_properties.tabColor = "229900"
+        
+        # Y sheets
+        wb.create_sheet("Y1.Fields")
+        ws4 = wb["Y1.Fields"]
+        ws4.sheet_properties.tabColor = "33AA00"
+        
+        # Z sheets
+        wb.create_sheet("Z1.Examples")
+        ws5 = wb["Z1.Examples"]
+        ws5.sheet_properties.tabColor = "44BB00"
+
+        # Data can be assigned directly to cells
+        ws['A1'] = 42
+
+        # Rows can also be appended
+        ws.append([1, 2, 3])
+
+        # Python types will automatically be converted
+        import datetime
+        ws['A2'] = datetime.datetime.now()
+
+        # Save the file
+        import os
+        wb.save(os.path.join(workdir, "01_document.xlsx"))
+
+        # Debug output
+        VaultExcel.__after_create_project(workdir)
+        print("ok")
+
+
+    
+    def __after_create_project(workdir):
+        from openpyxl import Workbook
+        wb = Workbook()
+
+        # grab the active worksheet
+        ws = wb.active
+
+        # Data can be assigned directly to cells
+        ws['A1'] = 42
+
+        # Rows can also be appended
+        ws.append([1, 2, 3])
+
+        # Python types will automatically be converted
+        import datetime
+        ws['A2'] = datetime.datetime.now()
+
+        # Save the file
+        import os
+        wb.save(os.path.join(workdir, "01_example.xlsx"))
+
+        # Debug output
+        print("after create project")
+
+
 
 if __name__ == "__main__":
-    import sys
-    create_project()
+    VaultExcel.create_project(".")
