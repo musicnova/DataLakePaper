f8295aac98e9cb486e10cb233cb50f02a8aa3e13
user <user@hadoop-m2@mos.ru>
2018-02-20 13:46:09 +0300

    init commit

--- anaconda/rebuild.sh
+++ anaconda/rebuild.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-conda-build datalakeplan
+rm -f /home/user/anaconda3/conda-bld/linux-64/datalakeplan-0.0.*-py35_0.tar.bz2
+conda-build --python 3.5 datalakeplan && conda install --use-local datalakeplan && anaconda upload /home/user/anaconda3/conda-bld/linux-64/datalakeplan-0.0.*-py35_0.tar.bz2
