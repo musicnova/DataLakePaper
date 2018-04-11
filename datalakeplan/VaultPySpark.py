566f0dee2a569d201bbf26a845c042ff04b67e7c
user <user@hadoop-m2@mos.ru>
2018-04-11 14:59:14 +0300

    make it better

--- datalakeplan/VaultPySpark.py
+++ datalakeplan/VaultPySpark.py
@@ -8,13 +8,15 @@ from pyspark.sql.functions import udf
 import re
 
 def get_arr_udf():
-  return udf(lambda txt: '' if txt is None else
-    ' '.join([re.sub(r"[^a-zа-яё]", "_",
-    (txt[i-1] if i - 1 >= 0 else '_').lower() + 
-     txt[i].lower() + (txt[i+1] if i + 1 <= len(txt) - 1 else '-').lower())
-    for i in range(0, len(txt))]), StringType())
-      key_udf = udf(lambda txt: '' if txt is None or len(txt)
-      else txt[0].lower(), StringType())
+    return udf(lambda txt: '' if txt is None else ' '.join([re.sub(r"[^a-zа-яё]", "_",
+                                            (txt[i-1] if i - 1 >= 0 else '_').lower() + 
+                                            txt[i].lower() +
+                                            (txt[i+1] if i + 1 <= len(txt) - 1 else '-').lower())
+                                     for i in range(0, len(txt))]), StringType())
+
+def get_key_udf():
+    return udf(lambda txt: '' if txt is None or len(txt)
+        else txt[0].lower(), StringType())
 
 def wfunc(a, b):
     x = set(a.split(' '))
@@ -25,11 +27,11 @@ def wfunc(a, b):
     return 0.0 if szx + szy == 0 else 1.0 * sz / (szx + szy) 
 
 def get_weight_udf():
-    udf(lambda a, b: wfunc(a, b), DoubleType())
+    return udf(lambda a, b: wfunc(a, b), DoubleType())
  
-def create_project():
-    print("ok")
+def ping():
+    print("pong")
 
 if __name__ == "__main__":
     import sys
-    create_project()
+    ping()
